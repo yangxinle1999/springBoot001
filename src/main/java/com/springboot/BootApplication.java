2028cd3bad2ae95a5f37f01b6e4b2af3444bdd09
yangxinle1999 <2842611637@qq.com>
2020-08-11 18:37:53 +0800

    增加功能

--- src/main/java/com/springboot/BootApplication.java
+++ src/main/java/com/springboot/BootApplication.java
@@ -3,6 +3,8 @@ package com.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+
 @SpringBootApplication
 public class BootApplication {
 
